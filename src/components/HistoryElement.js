01839444ea99157e56269f6649259eb91c26e051
leletu ngalo <lelethungalo@gmail.com>
2020-11-19 07:34:59 +0200

    added back

--- src/components/HistoryElement.js
+++ src/components/HistoryElement.js
@@ -1,20 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
 import { makeStyles, Typography, Paper, IconButton } from "@material-ui/core";
-import {DeleteOutlineOutlined} from "@material-ui/icons";
-import {projectDatabese} from "../firebase/config"
-const HistoryElement = ({id, title, price, positiveSign }) => {
+import { DeleteOutlineOutlined } from "@material-ui/icons";
+import { projectDatabese } from "../firebase/config";
+import AuthContext from "../context/auth/AuthContext";
+const HistoryElement = ({ id, title, price, positiveSign }) => {
+	const { user } = useContext(AuthContext);
 	const classes = useStyles(positiveSign);
 	const onDelete = () => {
 		try {
-			projectDatabese.collection("month1")
-            .doc(id)
-            .delete();
-            console.log("document succefully deleted");
-        } catch (error) {
-            console.error(error)
-        }
-	}
-	
+			projectDatabese.collection(user.email).doc(id).delete();
+			console.log("document succefully deleted");
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<Paper className={classes.wrap}>
 			<IconButton onClick={onDelete}>
@@ -29,7 +29,7 @@ const HistoryElement = ({id, title, price, positiveSign }) => {
 const useStyles = makeStyles({
 	wrap: {
 		width: "100%",
-		position:"relative",
+		position: "relative",
 		display: "flex",
 		alignItems: "center",
 		borderRight: positiveSign =>
@@ -38,12 +38,12 @@ const useStyles = makeStyles({
 		margin: "1em 0",
 		borderRadius: "0",
 	},
-	price:{
-		position:"absolute",
-		top:"50%",
-		right:"1em",
-		transform: "translateY(-50%)"
-	}
+	price: {
+		position: "absolute",
+		top: "50%",
+		right: "1em",
+		transform: "translateY(-50%)",
+	},
 });
 
 export default HistoryElement;
